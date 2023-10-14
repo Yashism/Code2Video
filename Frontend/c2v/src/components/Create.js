6f9d46eb40bf9628e5ea8858fcc068caa67fdead
jidaho <111899333+jidaho@users.noreply.github.com>
2023-10-14 21:37:06 +0000

    moving files

--- Frontend/c2v/src/components/Create.js
+++ Frontend/c2v/src/components/Create.js
@@ -1,24 +1,31 @@
+import ConceptButton from "./ConceptBtn";
+import CodeButton from "./CodeBtn";
+import "./Create.css"
+
 const InputBox = () => {
-    const [text, setText] = useState('');
-  
-    const handleChange = (event) => {
-      setText(event.target.value);
-    };
-  
-    return (
-      <div className="flex flex-col justify-center items-center">
-        <p className="text-lg font-semibold mb-2">Text</p>
-        <label htmlFor="textbox" className="text-lg font-semibold mb-2">
-          Enter your text:
-        </label>
-        <textarea
-          id="textbox"
-          className="w-full h-40 p-4 border border-gray-300 rounded-md"
-          value={text}
-          onChange={handleChange}
-        ></textarea>
-      </div>
-    );
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
   };
-  
-  export default InputBox
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="text-lg font-semibold mb-2">Text</p>
+      <textarea
+        id="textbox"
+        className="w-full h-40 p-4 border border-gray-300 rounded-md"
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+      ></textarea>
+
+      <div className="flex justify-between mt-4">
+        <ConceptButton />
+        <CodeButton />
+      </div>
+    </div>
+  );
+};
+
+export default InputBox;
