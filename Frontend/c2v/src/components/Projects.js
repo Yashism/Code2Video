140c365e8e1689f20c415ac33824df4f11b921ae
araohatkokate <araohatk@gmail.com>
2023-10-16 18:20:46 -0500

    finished projects page

--- Frontend/c2v/src/components/Projects.js
+++ Frontend/c2v/src/components/Projects.js
@@ -3,58 +3,97 @@ import React, { useState } from "react";
 
 const MyComponent = () => {
   const [projectName, setProjectName] = useState('');
+  const [projects, setProjects] = useState([]);
 
-  const handleClick = () => {
-    const projectName = prompt('Enter the name of the project:');
-    setProjectName(projectName);
+  const handleCreateProject = () => {
+    const newProjectName = prompt('Enter the name of the project:');
+    if (newProjectName) {
+      setProjects((prevProjects) => [
+        ...prevProjects,
+        { name: newProjectName, isEditing: false, style: { backgroundColor: 'black', color: 'white' } },
+      ]);
+    }
+  };
+
+  const handleDeleteProject = (projectIndex) => {
+    setProjects((prevProjects) => {
+      const updatedProjects = [...prevProjects];
+      updatedProjects.splice(projectIndex, 1);
+      return updatedProjects;
+    });
+  };
+
+  const handleEditProject = (projectIndex) => {
+    setProjects((prevProjects) => {
+      const updatedProjects = [...prevProjects];
+      updatedProjects[projectIndex].isEditing = true;
+      return updatedProjects;
+    });
+  };
+
+  const handleSaveProject = (projectIndex) => {
+    setProjects((prevProjects) => {
+      const updatedProjects = [...prevProjects];
+      updatedProjects[projectIndex].isEditing = false;
+      return updatedProjects;
+    });
   };
 
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">User Projects</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>
-        Create Project <span className="ml-2">&#43;</span>
+      <button className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleCreateProject}>
+        + Create Project
       </button>
-      {projectName && <p className="mt-4">New project name: {projectName}</p>}
+      {projects.length > 0 ? (
+        <ul className="mt-4">
+          {projects.map((project, index) => (
+            <li key={index} className="flex items-center mb-2">
+              {project.isEditing ? (
+                <>
+                  <input
+                    type="text"
+                    value={project.name}
+                    onChange={(e) => {
+                      const newName = e.target.value;
+                      setProjects((prevProjects) => {
+                        const updatedProjects = [...prevProjects];
+                        updatedProjects[index].name = newName;
+                        return updatedProjects;
+                      });
+                    }}
+                  />
+                  <button className="text-green-500 font-semibold" onClick={() => handleSaveProject(index)}>
+                    Save
+                  </button>
+                </>
+              ) : (
+                <>
+                  <div style={project.style} className="text-box p-2 rounded">
+                    {project.name}
+                  </div>
+                  <button className="text-red-500 font-semibold" onClick={() => handleDeleteProject(index)}>
+                    Delete
+                  </button>
+                  <button className="text-blue-500 font-semibold" onClick={() => handleEditProject(index)}>
+                    Edit
+                  </button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No projects found.</p>
+      )}
     </div>
   );
 };
 
 export default MyComponent;
 
-// const createProjectBtn = document.getElementById("createProjectBtn");
-// const projectList = document.getElementById("projectList");
 
-// createProjectBtn.addEventListener("click", () => {
-//   const projectName = prompt("Enter the name of the project:");
-//   if (projectName !== null && projectName.trim() !== "") {
-//     const projectBox = document.createElement("div");
-//     projectBox.className = "project-box";
-//     projectBox.innerHTML = `
-//       <span>${projectName}</span>
-//       <div class="edit-buttons">
-//         <button class="edit-btn">Edit</button>
-//         <button class="delete-btn">Delete</button>
-//       </div>
-//     `;
 
-//     projectBox.querySelector(".edit-btn").addEventListener("click", () => {
-//       const newName = prompt("Enter the new name of the project:");
-//       if (newName !== null && newName.trim() !== "") {
-//         projectBox.querySelector("span").textContent = newName;
-//       }
-//     });
-
-//     projectBox.querySelector(".delete-btn").addEventListener("click", () => {
-//       projectList.removeChild(projectBox);
-//     });
-
-//     projectList.appendChild(projectBox);
-//   }
-// });
-
-// export createProjectBtn;
-//export default MyComponent;
 
 
 
