cd3f19cddf317a918e27cac6c13ef9c2c54a9845
Yashism <yashvwaghmare2002@gmail.com>
2023-11-06 12:08:38 -0600

    V1.1

--- Frontend/c2v/src/components/App.js
+++ Frontend/c2v/src/components/App.js
@@ -37,10 +37,14 @@ function App() {
               <Route path="/Signup" element={<Signup />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/ForgotPassword" element={<ForgotPassword />} />
-              <Route path="/Create" element={<Create />} />
-              <Route path="/Projects" element={<Projects />} />
-
-              {/* Use PrivateRoute within a Route */}
+              <Route
+                path="/Create"
+                element={<PrivateRoute element={<Create />} />}
+              />
+              <Route
+                path="/Projects"
+                element={<PrivateRoute element={<Projects />} />}
+              />
               <Route
                 path="/Home"
                 element={<PrivateRoute element={<Home />} />}
