8376eeafd2b6c2d04e0d2c8ee48d2a2390ef3ba6
araohatkokate <araohatk@gmail.com>
2023-11-06 22:40:02 -0600

    merging stuff

--- Frontend/c2v/src/components/App.js
+++ Frontend/c2v/src/components/App.js
@@ -38,14 +38,12 @@ function App() {
               <Route path="/Signup" element={<Signup />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/ForgotPassword" element={<ForgotPassword />} />
-<<<<<<< HEAD
-              <Route path="/Create" element={<Create />} />
-              <Route path="/Projects" element={<Projects />} />
+              {/* <Route path="/Create" element={<Create />} />
+              <Route path="/Projects" element={<Projects />} /> */}
               <Route path="/Settings" element={<UserSettings />} />
 
 
               {/* Use PrivateRoute within a Route */}
-=======
               <Route
                 path="/Create"
                 element={<PrivateRoute element={<Create />} />}
@@ -54,7 +52,6 @@ function App() {
                 path="/Projects"
                 element={<PrivateRoute element={<Projects />} />}
               />
->>>>>>> cd3f19cddf317a918e27cac6c13ef9c2c54a9845
               <Route
                 path="/Home"
                 element={<PrivateRoute element={<Home />} />}
